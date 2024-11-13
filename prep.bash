fb9a646bc5b5690c57927eb49efc51c2ec28059a
Rick Venutolo <rick@venutolo.org>
2024-11-12 21:33:24 -0500

    create app data dir

--- prep.bash
+++ prep.bash
@@ -2,10 +2,12 @@
 
 set -euo pipefail
 
+mkdir --parents "${DOCKER_APP_DATA_DIR}"
+
 # backrest
 readonly restic_backup_dir='/backups/restic'
 sudo mkdir --parents "${restic_backup_dir}"
 sudo chown --recursive "${USER}:" "${restic_backup_dir}"
 
 # filebrowser
-touch "${DOCKER_APP_DATA_DIR:?}/filebrowser/filebrowser.db"
+touch "${DOCKER_APP_DATA_DIR}/filebrowser/filebrowser.db"
