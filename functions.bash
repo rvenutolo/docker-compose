cbd94adcbf3273501a1852e60381a3ad165094d8
Rick Venutolo <rick@venutolo.org>
2024-12-07 18:00:26 -0500

    copy cron file

--- functions.bash
+++ functions.bash
@@ -57,6 +57,46 @@ function create_dir() {
   done
 }
 
+# $1 = source file
+# $2 = destination file
+function root_copy_file() {
+  if [[ ! -f "$1" ]]; then
+    die "$1 does not exist"
+  fi
+  if [[ "$1" == "$2" ]]; then
+    die "File paths are the same"
+  fi
+  if [[ -f "$2" ]]; then
+    if cmp --silent "$1" "$2"; then
+      exit 0
+    fi
+  fi
+  log "Copying: $1 -> $2"
+  sudo mkdir --parents "$(dirname "$2")"
+  sudo cp "$1" "$2"
+  log "Copied: $1 -> $2"
+}
+
+# $1 = old file location
+# $2 = new file location
+function copy_file() {
+  if [[ ! -f "$1" ]]; then
+    die "$1 does not exist"
+  fi
+  if [[ "$1" == "$2" ]]; then
+    die "File paths are the same"
+  fi
+  if [[ -f "$2" ]]; then
+    if cmp --silent "$1" "$2"; then
+      exit 0
+    fi
+  fi
+  log "Copying: $1 -> $2"
+  mkdir --parents "$(dirname "$2")"
+  cp "$1" "$2"
+  log "Copied: $1 -> $2"
+}
+
 # $1 = file
 # $2 = content
 function write_file() {
