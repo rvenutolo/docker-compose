1436b9b15b9d66b77eedb2c65088cd89f21bd6c7
Rick Venutolo <rick@venutolo.org>
2025-02-03 19:36:01 -0500

    user service functions

--- functions.bash
+++ functions.bash
@@ -214,3 +214,21 @@ function set_env_file_var_if_not_defined() {
 function container_is_running() {
   [[ -n "$(docker ps --quiet --filter "name=^$1\$")" ]] && [[ "$(docker container inspect -f '{{.State.Status}}' "$1")" == 'running' ]]
 }
+
+# $1 = service unit file
+function user_service_unit_file_exists() {
+  systemctl --user list-unit-files --all --quiet "$1" > '/dev/null'
+}
+
+# $1 = service unit file
+function enable_user_service_unit() {
+  if user_service_unit_file_exists "$1"; then
+    if ! systemctl is-enabled --user --quiet "$1" && prompt_yn "Enable and start $1 user service?"; then
+      log "Enabling and starting $1 user service"
+      systemctl enable --now --user --quiet "$1"
+      log "Enabled and started $1 user service"
+    fi
+  else
+    log "User service unit files does not exist: $1"
+  fi
+}
